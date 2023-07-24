4bad11f61d72924b4a4af0af03098077db0b192c
The-alpha-male <ronnymunene@outlook.com>
2023-07-24 05:10:22 -0700

    function that produces output according to a format

--- task_0.c
+++ task_0.c
@@ -3,11 +3,36 @@
 #include <stdio.h>
 
 /**
+ * print_char - Print a single character.
+ * @c: The character to print.
+ * @count: A pointer to the count of characters printed.
+ */
+void print_char(char c, int *count)
+{
+	putchar(c);
+	(*count)++;
+}
+
+/**
+ * print_string - Print a string of characters.
+ * @str: The string to print.
+ * @count: A pointer to the count of characters printed.
+ */
+void print_string(const char *str, int *count)
+{
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		(*count)++;
+	}
+}
+
+/**
  * _printf - printf function
  * @format: character string
  * Return: Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -19,43 +44,35 @@ int _printf(const char *format, ...)
 
 	while (*format)
 	{
-	if (*format == '%')
-	{
-		format++;
-		switch (*format)
+		if (*format == '%')
 		{
-			case 'c':
-	character_arg = va_arg(args, int);
-	putchar(character_arg);
-	count++;
-	break;
-	case 's':
-	str_arg = va_arg(args, char *);
-	while (*str_arg)
-	{
-		putchar(*str_arg);
-		str_arg++;
-		count++;
-	}
-	break;
-	case '%':
-	putchar('%');
-	count++;
-	break;
-	default:
-	putchar('%');
-	putchar(*format);
-	count += 2;
-	break;
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					character_arg = va_arg(args, int);
+					print_char(character_arg, &count);
+					break;
+				case 's':
+					str_arg = va_arg(args, char *);
+					print_string(str_arg, &count);
+					break;
+				case '%':
+					print_char('%', &count);
+					break;
+				default:
+					print_char('%', &count);
+					print_char(*format, &count);
+					break;
+			}
 		}
+		else
+		{
+			print_char(*format, &count);
+		}
+		format++;
 	}
-else
-{
-	putchar(*format);
-	count++;
-}
-format++;
-}
-va_end(args);
+	va_end(args);
+
 	return (count);
 }
