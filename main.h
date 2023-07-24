6e6cebb0ae37d0ec789540432f5c00732e01e70d
The-alpha-male <ronnymunene@outlook.com>
2023-07-24 05:15:45 -0700

    function that produces output according to a format

--- main.h
+++ main.h
@@ -4,6 +4,20 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct print - struct for printing
+ * @type: type of argument
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *type;
+	int (*f)(va_list);
+} print_t;
+
 int _printf(const char *format, ...);
 
 int (*get_func(const char id))(va_list);
